d605fd3fd199b776c2a4d9f92a363a2e590ae8b3
Rahul Ghosal <rdghosal@gmail.com>
2023-07-19 08:07:39 -0400

    feat: update pkg name refs

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,10 @@ mod utils;
 fn main() {
     let args: Vec<String> = env::args().collect();
     if args.len() != 2 {
-        eprintln!("Usage: percy <path to .py files>");
+        eprintln!("Usage: nereus <path to .py files>");
         process::exit(-1);
     }
     let src = utils::read_files(Path::new(&args[1]), Option::None)
         .expect("Failed to read source file(s).");
-    fs::write("test.mmd", percy::transform(src)).expect("Failed to write output to file.");
+    fs::write("test.mmd", nereus::transform(src)).expect("Failed to write output to file.");
 }
