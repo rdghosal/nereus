43da7a7bbf5593d3acf709398f02e6a450a143ec
Rahul Ghosal <rdghosal@gmail.com>
2024-01-01 18:32:31 -0500

    feat: refac file reader to be idiomatic, allow read of single file

--- src/utils.rs
+++ src/utils.rs
@@ -1,18 +1,25 @@
 use std::{fs, io, path::Path};
 
-pub fn read_files(dir: &Path, src: Option<String>) -> Result<String, io::Error> {
+pub fn read_files(path: &Path, src: Option<String>) -> Result<String, io::Error> {
     let mut src = src.unwrap_or(String::new());
-    if dir.is_dir() {
-        for entry in fs::read_dir(dir)? {
-            let entry = entry?;
-            let path = entry.path();
-            if path.is_dir() {
-                src = read_files(&path, Option::Some(src))?;
-            } else {
-                let contents = fs::read_to_string(entry.path())?;
-                src.push_str(&contents);
+    if path.is_dir() {
+        for entry in fs::read_dir(path)? {
+            match entry {
+                Ok(e) => {
+                    let nested = e.path();
+                    if nested.is_dir() {
+                        src = read_files(&nested, Option::Some(src))?;
+                    } else {
+                        let contents = fs::read_to_string(nested)?;
+                        src.push_str(&contents);
+                    }
+                }
+                Err(..) => continue,
             }
         }
+    } else {
+        let contents = fs::read_to_string(path)?;
+        src.push_str(&contents);
     }
     Ok(src)
 }
